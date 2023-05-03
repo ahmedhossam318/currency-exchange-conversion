298031e5e787d1866da008995882962cb8118afe
ahmedhossam318 <57192217+ahmedhossam318@users.noreply.github.com>
2023-05-03 21:23:53 +0300

    add docker compose and Dockerfile for every microservices and add zipkin but it is not working

--- Api-Getway/src/main/java/com/getway/ApiGetwayApplication.java
+++ Api-Getway/src/main/java/com/getway/ApiGetwayApplication.java
@@ -2,7 +2,6 @@ package com.getway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class ApiGetwayApplication {
 
--- currency-exchange-services8000/currency-exchange-services/src/main/resources/scripts/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO currency_exchange (id, currency_from,currency_to, conversion_multiple,environment)
-VALUES
-    (10001, 'USD', 'INR', 65, ''),
-    (10002, 'EUR', 'USD', 1.2, ''),
-    (10003, 'GBP', 'USD', 1.4, '');
-select *from currency_exchange
